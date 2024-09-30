9c278a3b6d8ad18c87597c23bc7cea552975f032
Vlad G. <b100dian@gmail.com>
2024-09-30 15:24:53 +0300

    Update Alert service signatures

--- jscomps/AlertsService.js
+++ jscomps/AlertsService.js
@@ -7,6 +7,10 @@ const Ci = Components.interfaces;
 const { XPCOMUtils } = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
+XPCOMUtils.defineLazyServiceGetter(Services, "embedlite",
+                                    "@mozilla.org/embedlite-app-service;1",
+                                    "nsIEmbedAppService");
+
 Services.scriptloader.loadSubScript("chrome://embedlite/content/Logger.js");
 
 // -----------------------------------------------------------------------
@@ -14,15 +18,27 @@ Services.scriptloader.loadSubScript("chrome://embedlite/content/Logger.js");
 // -----------------------------------------------------------------------
 
 function AlertsService() {
-  Logger.debug("JSComp: AlertsService.js loaded");
+  dump("JSComp: AlertsService.js loaded\n");
 }
 
 AlertsService.prototype = {
   classID: Components.ID("{b98ab6b8-6c88-11e2-99bc-6745f7369235}"),
   QueryInterface: ChromeUtils.generateQI([Ci.nsIAlertsService]),
 
-  showAlertNotification: function(aImageUrl, aTitle, aText, aTextClickable, aCookie, aAlertListener, aName) {
+  showPersistentNotification: function (aPersistentData, aAlert, aAlertListener) {
+    dump("showPersistentNotification:" + aPersistentData + "\n")
+  },
+
+  showAlert: function(aAlert, aAlertListener) {
+    dump("showAlert:" + aAlert + "\n")
+  },
+
+  showAlertNotification: function(aImageUrl, aTitle, aText, aTextClickable, aCookie, aAlertListener, aName, aDir, aLang, aData, aPrincipal, aInPrivateBrowsing, aRequireInteraction) {
     dump("showAlertNotification: imgUrl:" + aImageUrl + ", title:" + aTitle + ", txt:" + aText + ", clickable:" + aTextClickable + ", cookie:" +  aCookie + ", listener:" + aAlertListener + ", Name:" + aName + "\n");
+  },
+
+  closeAlert: function(aName, aPrincipal) {
+    dump("closeAlert:" + aName + "\n");
   }
 };
 
